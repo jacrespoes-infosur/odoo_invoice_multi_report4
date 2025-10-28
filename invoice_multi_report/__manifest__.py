fdff8395eba2ff2982e83119ba1ce2d580a02b4f
jacrespoes-infosur <jacrespoes@gmail.com>
2025-10-28 17:14:01 +0100

    Add files via upload

---
+++ invoice_multi_report/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Invoice - Multiple Reports (A4 + Ticket)",
+    "version": "1.0",
+    "summary": "Provide two invoice reports: A4 and ticket",
+    "author": "ChatGPT",
+    "category": "Accounting",
+    "depends": ["account", "web"],
+    "data": [
+        "views/report_invoice_a4.xml",
+        "views/report_invoice_ticket.xml"
+    ],
+    "installable": True,
+    "application": False
+}
