c9e2cdb9fe862913b7da1fd69a5961bb1b18c834
jacrespoes-infosur <jacrespoes@gmail.com>
2025-10-28 17:45:25 +0100

    Update __manifest__.py

--- invoice_multi_report/__manifest__.py
+++ invoice_multi_report/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Invoice - Multiple Reports (A4 + Ticket)",
     "version": "1.0",
     "summary": "Provide two invoice reports: A4 and ticket",
-    "author": "ChatGPT",
+    "author": "Juan Antonio Crespo Guzman",
     "category": "Accounting",
     "depends": ["account", "web"],
     "data": [
